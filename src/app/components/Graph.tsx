f38d5bf14d4414132d952a9319e8f16d1019525b
Debasis Nath <debasisnath@INKACTXM332.local>
2023-08-30 01:29:54 +0530

    fixed build

--- src/app/components/Graph.tsx
+++ src/app/components/Graph.tsx
@@ -44,7 +44,7 @@ const Graph: React.FC<{ data: AppData }> = ({ data }) =>
             .data(root.links())
             .enter().append("path")
             .attr("class", "link")
-            .attr("d", d => `
+            .attr("d", (d: any) => `
                 M${d.target.y},${d.target.x}
                 C${d.source.y +  100},${d.target.x}
                  ${d.source.y + 200},${d.source.x}
@@ -56,17 +56,17 @@ const Graph: React.FC<{ data: AppData }> = ({ data }) =>
             .data(root.descendants())
             .enter().append("g")
             .attr("class", "node")
-            .attr("transform", d => `translate(${d.y},${d.x})`)
+            .attr("transform", (d: any) => `translate(${d.y},${d.x})`)
 
         node.append("circle")
             .attr("r", 2.5);
 
             node.append("text")
             .attr("dy", ".31em")
-            .attr("x", d => d.children ? -6 : 8) // Adjust x offset
+            .attr("x", (d: any) => d.children ? -6 : 8) // Adjust x offset
             .attr("y", 2) // Adjust y offset
-            .attr("text-anchor", d => d.children ? "end" : "start")
-            .text(d => d.data.name)
+            .attr("text-anchor", (d: any) => d.children ? "end" : "start")
+            .text((d: any) => d.data.name)
             .style("font-size", "10px");
 
     }, []);
